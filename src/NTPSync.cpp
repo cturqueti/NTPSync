583c7569b154c7c67512c826cc8f25164750a133
Carlos Augusto D'Orazio Turqueti <cturqueti@yahoo.com.br>
2025-05-19 10:02:20 -0300

    .

--- src/NTPSync.cpp
+++ src/NTPSync.cpp
@@ -268,7 +268,7 @@ bool NTPSync::resolveServer(NTPServer &server)
 
     IPAddress ip;
     WiFi.setDNS("8.8.8.8", "8.8.4.4");
-    WiFi.config() if (!WiFi.hostByName(server.hostname.c_str(), ip) || ip == IPAddress(0, 0, 0, 0))
+    if (!WiFi.hostByName(server.hostname.c_str(), ip) || ip == IPAddress(0, 0, 0, 0))
     {
         if (_logEnabled)
         {
